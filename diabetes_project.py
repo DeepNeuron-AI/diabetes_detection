b71a88d91ec5131d4ca85db68fdee39edabebea9
Johnny Liaw <jxlia3@student.monash.edu>
2018-09-02 23:22:11 +1000

    somechanges

--- diabetes_project.py
+++ diabetes_project.py
@@ -6,3 +6,5 @@ import tensorflow as tf
 filenames = ["data.csv"]
 record_defaults = [tf.float32] * 9   # Eight required float columns
 dataset = tf.contrib.data.CsvDataset(filenames, record_defaults)
+
+print('ello')
