3357f98d8b697b287deaf77778268f84e12cbfc4
Johnny Liaw <jxlia3@student.monash.edu>
2018-09-03 22:27:51 +1000

    imported pandas to partition dadta

--- diabetes_project.py
+++ diabetes_project.py
@@ -1,10 +1,15 @@
 #Loading libraries for use
 import numpy as np
 import tensorflow as tf
+import pandas as pd
+from sklearn.cross_validation import train_test_split
 
-
-filenames = ["data.csv"]
-record_defaults = [tf.float32] * 9   # Eight required float columns
-dataset = tf.contrib.data.CsvDataset(filenames, record_defaults)
-
-print('ello')
+data = pd.read_csv('data.csv')
+print(data.head(10))
+data['result'].replace(['tested_positive', 'tested_negative'], [1, 0], inplace = True)
+print(data.head(10))
+# filenames = ["data.csv"]
+# record_defaults = [tf.float32] * 9   # Eight required float columns
+# dataset = tf.contrib.data.CsvDataset(filenames, record_defaults, header = True)
+#
+# print(dataset)
